665bc6ece11849a3c0f37d50956148aeabacea47
Nathan Bull <nathan.richard.bull@gmail.com>
2022-04-13 07:29:50 -0400

    fixed print_board method to properly display board. Used find_index method to add player turns to vis_board properly. Make a list of identified issues left to solve

--- spec/game.rb
+++ spec/game.rb
@@ -12,21 +12,31 @@ attr_accessor :player_one_array, :player_two_array, :game_board
   self.play_game
   end
 
+  # add something to prevent error when column full
+  # break out of loop INSTANTLY when player wins (rather than full loop running first)
+  # need to capture DIAGONAL wins
+
   def play_game
   winner = nil
   while winner.nil?
-    # print_board(@game_board.board)
+    print_board(@game_board.vis_board, @game_board.height)
     puts "Player 1, select a column"
     puts "#{@game_board.width.join(', ')}"
-    player_one_sel = player_selection()
-    #spot_taken = test_board.board[player_sel].shift
+    player_one_sel = player_selection
     @player_one_array.push(@game_board.board[player_one_sel].shift)
+    @game_board.vis_board[player_one_sel][@game_board.vis_board[player_one_sel].find_index(' ')] = 'x'
+    p "vis board is #{@game_board.vis_board}"
+    p "game board is #{@game_board.board}"
+    p "player 1 array is #{@player_one_array}"
     winner = "Player 1" if check_for_match(@player_one_array)
+    print_board(@game_board.vis_board, @game_board.height)
     puts "Player 2, select a column"
     puts "#{@game_board.width.join(', ')}"
     player_two_sel = player_selection
-    #spot_taken = test_board.board[player_sel].shift
     @player_two_array.push(@game_board.board[player_two_sel].shift)
+    @game_board.vis_board[player_two_sel][@game_board.vis_board[player_two_sel].find_index(' ')] = 'o'
+    p "game board is #{@game_board.board}"
+    p "player 2 array is #{@player_two_array}"
     winner = "Player 2" if check_for_match(@player_two_array)
   end
   p "#{winner} wins!"
--- spec/game_board.rb
+++ spec/game_board.rb
@@ -1,43 +1,56 @@
 class GameBoard
-  attr_accessor :board, :width, :height
-  
-  def initialize(width, height)
-    @width = (1..width).to_a
-    @height = height
-    @board = build_board(width, height)
-    @vis_board = build_vis_board(@board)
-    p "board is #{@board}" # need non-destructive way of creating vis_board
-    p "vis_board is #{@vis_board}"
-  end
-  
-  def build_board(width, height)
-    board = Hash.new(0)
-    (1..width).each do |row_val|
-      array = []
-      (1..height).each do |col_val|
-        array.push([row_val, col_val])
-      end
-      board[row_val] = array
-    end
-    board
-  end
+attr_accessor :board, :vis_board, :width, :height
 
-  # accepts board and creates a board of empty spaces
-  def build_vis_board(board_hash)
-    board_hash.each do |key, val|
-      val.map! { |num| ' ' }
-    end
-  end
+def initialize(width, height)
+  @width = (1..width).to_a
+  @height = height
+  @board = build_board(width, height)
+  @vis_board = build_vis_board(width, height)
+  #p "board is #{@board}" # need non-destructive way of creating vis_board
+  #p "vis_board is #{@vis_board}"
+end
 
-  # accepts empty space board and prints to console
-  def print_board(board_hash)
-    board_hash.each do |key, val|
-      print_line = []
-      val.each do |num|
-        print_line.push(num)
-      end
-      new = print_line.join(' | ')
-      puts new
+def build_board(width, height)
+  board = Hash.new(0)
+  (1..width).each do |row_val|
+    array = []
+    (1..height).each do |col_val|
+      array.push([row_val, col_val])
     end
+    board[row_val] = array
   end
+  board
+end
+
+# accepts board and creates a board of empty spaces
+def build_vis_board(width, height) #(board_hash)
+  board = Hash.new(0)
+  (1..width).each do |row_val|
+    array = []
+    (1..height).each do |col_val|
+      array.push(' ')
+    end
+    board[row_val] = array
+  end
+  board
+  #board_hash.each do |key, val|
+  #  val.map { |num| ' ' }
+  #end
+end
+
+# accepts empty space board and prints to console
+def print_board(board_hash, height)
+  matrix = []
+   (1..height).reverse_each do |row|
+     new_row = []
+     board_hash.each do |_key, col|
+       new_row.push(col[row - 1])
+     end
+     matrix.push(new_row)
+   end
+   matrix.each do |row|
+     p row.join(' | ')
+   end    
+ end
+
 end
